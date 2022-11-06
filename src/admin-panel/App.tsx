cb1805369083cad8e4f294824ddd31200d96af5e
clmrv <everay@wp.pl>
2022-11-06 14:58:10 +0100

    Add mui and sign in page

--- src/admin-panel/App.tsx
+++ src/admin-panel/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import SignIn from "./pages/SignIn";
+
 function App() {
-  return <div>panel</div>;
+  const theme = createTheme();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <SignIn />
+    </ThemeProvider>
+  );
 }
 
 export default App;
