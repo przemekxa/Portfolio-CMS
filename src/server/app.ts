29c55d9e19209ca26443cd4346a88fb982bdaa6b
Przemysław Ambroży <przemek19986@gmail.com>
2022-11-20 21:33:49 +0100

    Add login/logout endpoints

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,9 +1,21 @@
-import fastify from "fastify";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
 import fastifyStatic from "@fastify/static";
 import fastifyView from "@fastify/view";
+import fastifySession from "@fastify/session";
+import fastifyCookie from "@fastify/cookie";
 import autoLoad from "@fastify/autoload";
 import path from "path";
 import handlebars from "handlebars";
+
+declare module "fastify" {
+  interface Session {
+      authenticated: boolean
+  }
+
+  interface FastifyInstance {
+    authenticated: (request: FastifyRequest, response: FastifyReply) => Promise<void>
+  }
+}
 
 handlebars.registerHelper("eq", (context, options) => {
   return context === options;
@@ -27,6 +39,7 @@ const getApp = async () => {
     decorateReply: false,
   });
 
+  // Handlebars
   app.register(fastifyView, {
     engine: {
       handlebars: handlebars,
@@ -42,8 +55,23 @@ const getApp = async () => {
     },
   });
 
+  // Routes
   app.register(autoLoad, {
     dir: path.join(__dirname, "routes"),
+  });
+
+  // Authentication
+  app.register(fastifyCookie);
+  app.register(fastifySession, {
+    secret: "Vv6ZPSeui5PN14vxZYAui4L1blNqirzO/a/ZLgR783/CFs8VafGAgLi6tuL500F1",
+    cookie: {
+      secure: false,
+    },
+  });
+  app.decorate("authenticated", async (request: FastifyRequest, reply: FastifyReply) => {
+    if (request.session.authenticated !== true) {
+      reply.status(401).send();
+    }
   });
 
   return app;
---
+++ src/server/routes/api/auth/index.ts
@@ -0,0 +1,21 @@
+import { FastifyPluginAsync, FastifyRequest } from "fastify";
+import mongo from "../../../mongo";
+import { TestModel } from "../../../../common/collections";
+
+const auth: FastifyPluginAsync = async (fastify): Promise<void> => {
+  fastify.post("/login", async (request: FastifyRequest<{ Body: { username: string, password: string }}>, reply) => {
+    if(request.body.username === "admin" && request.body.password == "admin") {
+      request.session.authenticated = true;
+      reply.code(200);
+    } else {
+      reply.code(401);
+    }
+  });
+
+  fastify.post("/logout", { preHandler: fastify.authenticated }, async (request, reply) => {
+    request.session.authenticated = false;
+    reply.code(200).send();
+  })
+};
+
+export default auth;
