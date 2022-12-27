16b2bc3b77349d54a74cc5c91752ba687913f08d
Przemysław Ambroży <przemek19986@gmail.com>
2022-12-27 23:07:44 +0100

    Add missing default sections

--- src/common/defaultSections.ts
+++ src/common/defaultSections.ts
@@ -1,9 +1,17 @@
 import { v4 as uuidv4 } from "uuid";
-import { Header, Paragraph, RichText, SectionTypeId } from "./sections";
+import { SectionTypeId, PageHeader, Header, OneImage, TwoImages, Paragraph, RichText, ParagraphImage, Contact, SubpageCarousel } from "./sections";
 
 export const createDefaultSection = (sectionType: SectionTypeId) => {
   const id = uuidv4();
   switch (sectionType) {
+    case "pageHeader":
+      const pageHeader: PageHeader = {
+        id,
+        type: "pageHeader",
+        showCard: false,
+      };
+      return pageHeader;
+
     case "header":
       const headerSection: Header = {
         id,
@@ -11,6 +19,32 @@ export const createDefaultSection = (sectionType: SectionTypeId) => {
         header: "",
       };
       return headerSection;
+
+    case "1image":
+      const oneImageSection: OneImage = {
+        id,
+        type: "1image",
+        title: "",
+        src: ""
+      };
+      return oneImageSection;
+
+    case "2images":
+      const twoImagesSection: TwoImages = {
+        id,
+        type: "2images",
+        images: [
+          {
+            title: "",
+            src: "",
+          },
+          {
+            title: "",
+            src: "",
+          },
+        ]
+      };
+      return twoImagesSection;
 
     case "paragraph":
       const paragraphSection: Paragraph = {
@@ -27,6 +61,47 @@ export const createDefaultSection = (sectionType: SectionTypeId) => {
         value: "",
       };
       return richText;
+
+    case "paragraphImage":
+      const paragraphImage: ParagraphImage = {
+        id,
+        type: "paragraphImage",
+        paragraph: "",
+        image: "",
+      };
+      return paragraphImage;
+
+    case "subpageCarousel":
+      const subpageCarousel: SubpageCarousel = {
+        id,
+        type: "subpageCarousel"
+      };
+      return subpageCarousel;
+
+    case "contact":
+      const contact: Contact = {
+        id,
+        type: "contact",
+        phone: "",
+        email: "",
+        address: "",
+        socialHeader: "",
+        social: [
+          {
+            icon: "facebook",
+            href: "",
+          },
+          {
+            icon: "twitter",
+            href: "",
+          },
+          {
+            icon: "linkedin",
+            href: "",
+          },
+        ]
+      };
+      return contact;
 
     default:
       const _exhaustiveCheck: never = sectionType;
