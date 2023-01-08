e1260130b8664c4ffc751d7a97ad2bc3db8e0fba
Przemysław Ambroży <przemek19986@gmail.com>
2023-01-08 16:39:53 +0100

    Add reorderable menu

--- src/admin-panel/dndHelpers.ts
+++ src/admin-panel/dndHelpers.ts
@@ -1,4 +1,4 @@
-export const reorder = (list: any[], startIndex: number, endIndex: number) => {
+export const reorder = <T>(list: T[], startIndex: number, endIndex: number) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed);
